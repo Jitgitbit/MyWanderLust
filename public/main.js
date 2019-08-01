5f5523c686d13071d54ebecfdfbb63ae8031b734
Thierry Dekelver <rowowolf@gmail.com>
2019-08-01 20:51:36 +0200

    filing

---
+++ public/main.js
@@ -0,0 +1,90 @@
+// Foursquare API Info
+const clientId = '0D30P0CAGAO1I55JSSIPMVH11ZV0OHVIDK4E1PARKP3425WH';
+const clientSecret = 'K5UWIE1JBXANENM3KRFSOVITP0FLLCPFM4FG1HXNZIAVZN2X';
+const url = 'GET https://api.foursquare.com/v2/venues/explore?near=';
+
+// APIXU Info
+const apiKey = 'c1e10c64a3094cef967184205193107';
+const forecastUrl = 'http://api.apixu.com/v1/forecast.json?key=';
+
+// Page Elements
+const $input = $('#city');
+const $submit = $('#button');
+const $destination = $('#destination');
+const $container = $('.container');
+const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
+const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weather4")];
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// Add AJAX functions here:
+const getVenues = async () => {
+  const city = $input.val();
+  const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20190801`;
+  try{
+    const response = await fetch(urlToFetch);
+    if(response.ok){
+      console.log(response);
+    const jsonResponse = await response.json();
+      console.log(jsonResponse);
+      const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+      console.log(venues);
+      return venues;
+  	}else{
+      throw new Error('Request failed!');
+    };  
+  }catch(error){
+    console.log(error.message);
+  };
+	
+}
+
+const getForecast = async () => {
+  const urlToFetch = `${getForecast}${apiKey}&q=${$input.val()}&days=4&hour=11`;
+  try{
+    const response = await fetch(urlToFetch);
+    if(response.ok){
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+      const days = jsonResponse.forecast.forecastday;
+      return days;
+    }else{
+      throw new Error('Request failed!');
+    }
+  }catch(error){
+    console.log(error.message);
+  }
+
+}
+
+
+// Render functions
+const renderVenues = (venues) => {
+  $venueDivs.forEach(($venue, index) => {
+    const venue = venues[index];
+    const venueIcon = venue.categories[0].icon;
+    const venueImgSrc = `${venueIcon.prefix}bg_64${venueIcon.suffix}`;
+    let venueContent = createVenueHTML(venue.name, venue.location, venueImgSrc);
+    $venue.append(venueContent);
+  });
+  $destination.append(`<h2>${venues[0].location.city}</h2>`);
+}
+
+const renderForecast = (days) => {
+  $weatherDivs.forEach(($day, index) => {
+    const currentDay = days[index];
+    let weatherContent = createWeatherHTML(currentDay);
+    $day.append(weatherContent);
+  });
+}
+
+const executeSearch = () => {
+  $venueDivs.forEach(venue => venue.empty());
+  $weatherDivs.forEach(day => day.empty());
+  $destination.empty();
+  $container.css("visibility", "visible");
+  getVenues().then(venues => renderVenues(venues));
+  getForecast().then(forecast => renderForecast(forecast));
+  return false;
+}
+
+$submit.click(executeSearch)
